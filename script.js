2d678ecd64a4e12d95b81bf753b9ee9d545ac9d0
Jayzir Martinez <jayzirslaider18@gmail.com>
2026-04-24 14:49:54 -0500

    Cambio de vídeo de YouTube

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-// Seleccionamos todos los enlaces de la navegación que tengan un '#'
 const navLinks = document.querySelectorAll('nav a[href^="#"]');
 const backToTopButton = document.getElementById("GoToTop");
 const sideMenu = document.getElementById("sideMenu");
 
 navLinks.forEach(link => {
     link.addEventListener('click', function (e) {
-        // 1. Evitamos el comportamiento por defecto (que recarga o salta bruscamente)
         e.preventDefault();
 
-        // 2. Obtenemos el ID de la sección a la que queremos ir
         const targetId = this.getAttribute('href');
         const targetSection = document.querySelector(targetId);
 
-        // 3. Si la sección existe, nos desplazamos suavemente
         if (targetSection) {
             targetSection.scrollIntoView({
                 behavior: 'smooth',
@@ -22,7 +18,6 @@ navLinks.forEach(link => {
     });
 });
 
-// Mostrar el botón cuando el usuario baja 300px desde el tope
 window.onscroll = function () {
     const scrollPos = document.body.scrollTop > 300 || document.documentElement.scrollTop > 300;
 
@@ -35,7 +30,6 @@ window.onscroll = function () {
     }
 };
 
-// Al hacer clic, sube suavemente
 backToTopButton.addEventListener("click", function () {
     window.scrollTo({
         top: 0,
