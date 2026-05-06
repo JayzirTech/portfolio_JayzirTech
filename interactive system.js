1518848d60d288b0108450f542672eb7836faf9b
Jayzir Martinez <jayzirslaider18@gmail.com>
2026-05-06 15:36:56 -0500

    Correción en el pront del nombre

--- interactive system.js
+++ interactive system.js
@@ -1,9 +1,12 @@
-const nameEntered = prompt("Please, enter your name: ");
-let ageEntered;
+let nameEntered
+let ageEntered
+
+do {
+    nameEntered = prompt("Please, enter your name: ");
+} while (!nameEntered)
 
 do {
     ageEntered = prompt(`Welcome ${nameEntered}, this is Jayzir Martinez's WebSite. Please, enter your age: `);
     alert((ageEntered === "" || isNaN(ageEntered) || ageEntered <= 0) ? "Invalid Number" : (ageEntered >= 18) ? "You Are Adult" : "You Are Young")
 
 } while (ageEntered === "" || isNaN(ageEntered) || ageEntered <= 0);
-
