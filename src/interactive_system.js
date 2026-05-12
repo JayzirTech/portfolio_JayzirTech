ccf7f1307130b56dd4e2c85690696cf3966b5962
Jayzir Martinez <jayzirslaider18@gmail.com>
2026-05-12 12:34:41 -0500

    Correción de error (No aparecía el nombre)

--- src/interactive_system.js
+++ src/interactive_system.js
@@ -2,8 +2,6 @@
 let nameEntered
 let ageEntered
 
-let minorThan18 = `Hello ${nameEntered}, you are under 18. Keep learning and enjoying coding!`
-let majorThan18 = `Hello ${nameEntered}, You are of legal age. Get ready for great opportunities in the world of programming!`
 const invalidNumber = "Error: Please, Enter a valid age number."
 
 //Asks for the username. If you don't enter it, it asks for it again.
@@ -11,11 +9,14 @@ do {
     nameEntered = prompt("Please, enter your name:").trim();
 } while (!nameEntered)
 
+let minorThan18 = `Hello ${nameEntered}, you are under 18. Keep learning and enjoying coding!`
+let majorThan18 = `Hello ${nameEntered}, You are of legal age. Get ready for great opportunities in the world of programming!`
+
 //Asks for the user's age.
 //If the user enters an invalid number (nothing, text, or a decimal), it asks again.
 do {
     ageEntered = prompt(`Welcome ¡${nameEntered}!, this is Jayzir Martinez's WebSite. Please, enter your age:`);
 
-    (isNaN(ageEntered) || ageEntered <= 0 || !Number.isInteger(Number(ageEntered))) ? console.error(invalidNumber) : (ageEntered >= 18) ? console.log(majorThan18) : console.warn(minorThan18)
+    (ageEntered <= 0 || !Number.isInteger(Number(ageEntered))) ? console.error(invalidNumber) : (ageEntered >= 18) ? console.log(majorThan18) : console.warn(minorThan18)
 
-} while (isNaN(ageEntered) || ageEntered <= 0 || !Number.isInteger(Number(ageEntered)));
+} while (ageEntered <= 0 || !Number.isInteger(Number(ageEntered)));
