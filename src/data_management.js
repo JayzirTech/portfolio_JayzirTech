946be2a59e6322dac5381d3aae0fc5cd1bfaf184
Jayzir Martinez <jayzirslaider18@gmail.com>
2026-05-14 15:52:27 -0500

    Empezando M3S2...

---
+++ src/data_management.js
@@ -0,0 +1,62 @@
+const inventoryMap = new Map([
+    [
+        "1001", {
+            nombre: "Cuaderno Rayado 100H",
+            precio: 5500,
+            categoria: "Papelería"
+        }],
+    [
+        "1002", {
+            nombre: "Caja de Colores x24",
+            precio: 12000,
+            categoria: "Papelería"
+        }],
+    [
+        "2002", {
+            nombre: "Resaltador Fluorescente",
+            precio: 2800,
+            categoria: "Útiles de Oficina"
+        }],
+    [
+        "3001", {
+            nombre: "Cartulina de Colores",
+            precio: 800,
+            categoria: "Manualidades"
+        }],
+    [
+        "3002", {
+            nombre: "Silicona Líquida 250ml",
+            precio: 8500,
+            categoria: "Manualidades"
+        }],
+    [
+        "2001", {
+            nombre: "Pegante en Barra 40g",
+            precio: 3500,
+            categoria: "Útiles de Oficina"
+        }],
+    [
+        "4001", {
+            nombre: "Carpeta de Presentación",
+            precio: 1500,
+            categoria: "Archivo"
+        }],
+    [
+        "4002", {
+            nombre: "Ganchos Legajadores x20",
+            precio: 2200,
+            categoria: "Archivo"
+        }],
+    [
+        "5001", {
+            nombre: "Calculadora Científica",
+            precio: 45000,
+            categoria: "Tecnología"
+        }],
+    [
+        "5002", {
+            nombre: "Memoria USB 64GB",
+            precio: 28000,
+            categoria: "Tecnología"
+        }]
+]);
